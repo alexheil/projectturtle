35fc28826207ef31229c2dc91ba7b0a0037521b5
alexheil <aheil4250@gmail.com>
2019-09-07 19:44:17 -0400

    users

---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ApplicationRecord
+  extend FriendlyId
+  friendly_id :username, use: :slugged
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  attr_accessor :login
+
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9]+\Z/i }
+  validate :validate_username
+
+  before_save :should_generate_new_friendly_id?, if: :username_changed?
+  before_save :downcase_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  private
+
+    def should_generate_new_friendly_id?
+      username_changed?
+    end
+
+    def downcase_username
+      self.username = username.downcase
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  if Rails.env.production?
+    mount Shrine.presign_endpoint(:cache) => '/presign'
+  end
+
   root 'static_pages#home'
+
+  devise_for :users, controllers: { sessions: "users/sessions", passwords: "users/passwords", registrations: "users/registrations", confirmations: "users/confirmations",  unlocks: "users/unlocks"}
+
+  devise_scope :user do
+    get 'sign_out', to: 'users/sessions#destroy', path: 'sign-out'
+    get 'sign_in', to: 'users/sessions#new', path: 'sign-in'
+    get 'register', to: 'users/registrations#new'
+  end
 
 end
